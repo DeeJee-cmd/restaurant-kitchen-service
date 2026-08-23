29a013bab5991cf0d71e88e16fffb2a00931d708
Pasha Liashenko <zedxi236@ukr.net>
2026-08-23 22:05:16 +0300

    remove redundant AbstractUser field overrides

--- apps/cooks/models.py
+++ apps/cooks/models.py
@@ -3,12 +3,7 @@ from django.db import models
 
 # Create your models here.
 class Cook(AbstractUser):
-    years_on_experience = models.PositiveSmallIntegerField(default=0)
-    username = models.CharField(max_length=50, unique=True)
-    email = models.EmailField(unique=True)
-    password = models.CharField(max_length=100)
-    first_name = models.CharField(max_length=50)
-    last_name = models.CharField(max_length=50)
+    years_of_experience = models.PositiveSmallIntegerField(default=0)
 
     def __str__(self):
         return f"{self.first_name} {self.last_name} ({self.username})"

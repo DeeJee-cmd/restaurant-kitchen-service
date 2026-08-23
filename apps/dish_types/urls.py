93a42d639f2ce52bbe60dae59c62277e75e3de99
Pasha Liashenko <zedxi236@ukr.net>
2026-08-23 21:28:35 +0300

    feat: add dish_types forms, urls and CRUD views

--- apps/dish_types/urls.py
+++ apps/dish_types/urls.py
@@ -2,10 +2,38 @@ from django.urls import path
 
 from apps.dish_types.views import (
     DishTypeListView,
+    DishTypeDetailView,
+    DishTypeCreateView,
+    DishTypeUpdateView,
+    DishTypeDeleteView,
 )
 
 app_name = "dish_types"
 
 urlpatterns = [
-    path("dish-types/", DishTypeListView.as_view(), name="dish-type-list"),
+    path(
+        "dish-types/",
+        DishTypeListView.as_view(),
+        name="dish-type-list"
+    ),
+    path(
+        "dish-types/<int:pk>/",
+        DishTypeDetailView.as_view(),
+        name="dish-type-detail"
+    ),
+    path(
+        "dish-types/create/",
+        DishTypeCreateView.as_view(),
+        name="dish-type-create"
+    ),
+    path(
+        "dish-types/<int:pk>/update/",
+        DishTypeUpdateView.as_view(),
+        name="dish-type-update"
+    ),
+    path(
+        "dish-types/<int:pk>/delete/",
+        DishTypeDeleteView.as_view(),
+        name="dish-type-delete"
+    )
 ]

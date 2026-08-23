dad1e75b763cf25423488b6517abffcc78c02396
Pasha Liashenko <zedxi236@ukr.net>
2026-08-23 21:50:21 +0300

    added Cook model extending AbstractUser

--- apps/cooks/models.py
+++ apps/cooks/models.py
@@ -1,3 +1,14 @@
+from django.contrib.auth.models import AbstractUser
 from django.db import models
 
 # Create your models here.
+class Cook(AbstractUser):
+    years_on_experience = models.PositiveSmallIntegerField(default=0)
+    username = models.CharField(max_length=50, unique=True)
+    email = models.EmailField(unique=True)
+    password = models.CharField(max_length=100)
+    first_name = models.CharField(max_length=50)
+    last_name = models.CharField(max_length=50)
+
+    def __str__(self):
+        return f"{self.first_name} {self.last_name} ({self.username})"

333a5bfd209da792e8b8d337bc6ac706c2b97c14
Pasha Liashenko <zedxi236@ukr.net>
2026-08-23 17:38:07 +0300

    refactor: recreate dish_types app under apps/ directory

--- apps/dish_types/apps.py
+++ apps/dish_types/apps.py
@@ -1,6 +1,6 @@
 from django.apps import AppConfig
 
 
-class DishTypeConfig(AppConfig):
-    default_auto_field = 'django.db.models.BigAutoField'
-    name = 'apps.dish_types'
+class DishTypesConfig(AppConfig):
+    default_auto_field = "django.db.models.BigAutoField"
+    name = "apps.dish_types"

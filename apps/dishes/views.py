e35eb73ad63dd878c730dbc8c90a5d69925a11bb
Pasha Liashenko <zedxi236@ukr.net>
2026-08-24 22:38:11 +0300

    refactor: rename dish app to dishes and clean old template typos

--- apps/dishes/views.py
+++ apps/dishes/views.py
@@ -1,3 +1,24 @@
-from django.shortcuts import render
+from django.contrib.auth.mixins import LoginRequiredMixin
+from django.views import generic
+
+from apps.dishes.forms import DishForm
+from apps.dishes.models import Dish
+
 
 # Create your views here.
+class DishesListView(LoginRequiredMixin, generic.ListView):
+    model = Dish
+    paginate_by = 5
+    queryset = Dish.objects.select_related("dish_type")
+    template_name = "kitchen/dishes/dish_list.html"
+
+
+class DishCreateView(LoginRequiredMixin, generic.CreateView):
+    model = Dish
+    form_class = DishForm
+    template_name = "kitchen/dishes/dish_form.html"
+
+
+class DishDetailView(LoginRequiredMixin, generic.DetailView):
+    model = Dish
+    template_name = "kitchen/dishes/dish_detail.html"

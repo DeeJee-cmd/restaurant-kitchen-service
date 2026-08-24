8f79d12d687ee0ce4edc35f6e43bd25562f092f3
Pasha Liashenko <zedxi236@ukr.net>
2026-08-24 12:21:16 +0300

    added model Dish

--- apps/dishes/models.py
+++ apps/dishes/models.py
@@ -1,4 +1,23 @@
 from django.db import models
 
+from apps.cooks.models import Cook
+from apps.dish_types.models import DishType
+
+
 # Create your models here.
-c
+class Dish(models.Model):
+    name = models.CharField(max_length=100)
+    description = models.TextField()
+    price = models.DecimalField(max_digits=10, decimal_places=2)
+    dish_type = models.ForeignKey(
+        DishType,
+        on_delete=models.CASCADE,
+        related_name='dishes',
+    )
+    cooks = models.ManyToManyField(
+        Cook,
+        related_name='dishes',
+    )
+
+    def __str__(self):
+        return self.name

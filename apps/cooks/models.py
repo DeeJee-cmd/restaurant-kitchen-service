97f42dbae6a0cba085191606f6f97f53331d75f7
Pasha Liashenko <zedxi236@ukr.net>
2026-08-29 18:15:05 +0300

    Add default ordering to Cook, DishType and Dish models

--- apps/cooks/models.py
+++ apps/cooks/models.py
@@ -1,9 +1,12 @@
 from django.contrib.auth.models import AbstractUser
 from django.db import models
 
-# Create your models here.
+
 class Cook(AbstractUser):
     years_of_experience = models.PositiveSmallIntegerField(default=0)
+
+    class Meta:
+        ordering = ["username"]
 
     def __str__(self):
         return f"{self.first_name} {self.last_name} ({self.username})"

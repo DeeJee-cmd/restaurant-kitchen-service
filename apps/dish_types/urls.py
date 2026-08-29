0c5f4db945b99d85afd182eee4e2cc22d8c6990d
Pasha Liashenko <zedxi236@ukr.net>
2026-08-29 18:19:01 +0300

    Fix long lines in urls.py files (flake8 E501)

--- apps/dish_types/urls.py
+++ apps/dish_types/urls.py
@@ -12,6 +12,14 @@ app_name = "dish_types"
 urlpatterns = [
     path("", DishTypeListView.as_view(), name="dish-type-list"),
     path("create/", DishTypeCreateView.as_view(), name="dish-type-create"),
-    path("<int:pk>/update/", DishTypeUpdateView.as_view(), name="dish-type-update"),
-    path("<int:pk>/delete/", DishTypeDeleteView.as_view(), name="dish-type-delete")
+    path(
+        "<int:pk>/update/",
+        DishTypeUpdateView.as_view(),
+        name="dish-type-update"
+    ),
+    path(
+        "<int:pk>/delete/",
+        DishTypeDeleteView.as_view(),
+        name="dish-type-delete"
+    )
 ]
